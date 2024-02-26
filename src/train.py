5a22b52eb49ee7713e28c5462638b178c02d8a8b
Urban Potato <veryprogramming@gmail.com>
2024-02-26 21:39:47 +0700

    Code written

--- src/train.py
+++ src/train.py
@@ -10,13 +10,11 @@ def main(repo_path):
     train_csv_path = repo_path / "data/prepared/train.csv"
     train_data, labels = load_data(train_csv_path)
 
-    sgd = SGDClassifier(random_state=91, max_iter=500)
-    trained_model = sgd.fit(train_data, labels)
+    # sgd = SGDClassifier(random_state=91, max_iter=500)
+    # trained_model = sgd.fit(train_data, labels)
 
-    # rf = RandomForestClassifier(
-    #     random_state=91, max_depth=5, n_estimators=10, max_features=1
-    # )
-    # trained_model = rf.fit(train_data, labels)
+    rf = RandomForestClassifier(random_state=91)
+    trained_model = rf.fit(train_data, labels)
 
     # svc = SVC(random_state=91, gamma=0.001)
     # trained_model = svc.fit(train_data, labels)
